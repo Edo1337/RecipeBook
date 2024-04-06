fb8a25da7d1d0283b3782dad98adda08c158045d
eduardgladkov <eduard.gladkov.00@gmail.com>
2024-04-06 16:09:45 +0300

    Добавил TokenController и реализовал метод RefreshToken в нем

---
+++ RecipeBook.Api/Controllers/TokenController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeBook.Application.Services;
+using RecipeBook.Domain.Dto;
+using RecipeBook.Domain.Interfaces.Services;
+using RecipeBook.Domain.Result;
+
+namespace RecipeBook.Api.Controllers
+{
+    public class TokenController : Controller
+    {
+        private readonly ITokenService _tokenService;
+
+        public TokenController(ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BaseResult<TokenDto>>> RefreshToken([FromBody] TokenDto dto)
+        {
+            var response = await _tokenService.RefreshToken(dto);
+            if (response.isSuccess)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response);
+        }
+    }
+}
