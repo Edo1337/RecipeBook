08489e1cc651bee3f3305303bccc80115f4b261b
eduardgladkov <eduard.gladkov.00@gmail.com>
2024-04-08 03:21:24 +0300

    Начал добавлять Exception Middleware в проект

---
+++ RecipeBook.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace RecipeBook.Api.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            _logger.LogError(ex, ex.Message); //Не уверен что верно сделал туточки
+        }
+    }
+}
